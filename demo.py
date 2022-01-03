ad54ebf91314762e143fcf342fa96310d1593bfd
AFishyOcean <Oo*2=31.9988>
2022-01-03 12:45:20 -0500

    did snow being

--- demo.py
+++ demo.py
@@ -2,8 +2,17 @@
 import pygame, sys
 from pygame.locals import *
 
+import snowbeing
 
+pygame.init() # usually first command
+main_surface = pygame.display.set_mode((600, 400)) # sets window size
+main_surface.fill((135, 206, 235))
 
+snow = snowbeing.Snowbeing(main_surface)
+snow.body()
+snow.hat()
+
+pygame.display.update() # must put in this command to see everything
 
 # Standard starting while loop for Pygame programs.
 # Much more can be added to this as needed.
